498553fb9841944fa3f3fbfa10210cf94d3c55d5
Zeeshan Ahmed <anonymousguyx@gmail.com>
2017-05-14 18:25:40 +0500

    Font-end added for default Featured Image.
    If the posts are not contain the featured images than you can add a default image for them. 
    The posts that are with featured image will not be infected and they'll continuously using the featured image that is added via edit post.

--- facebook-opengraph-tags/facebook-opengraph-tags.php
+++ facebook-opengraph-tags/facebook-opengraph-tags.php
@@ -5,6 +5,8 @@
 * Version: 1.0
 *Author: Zeeshan Ahmed
 */
+
+// Header Scripts
 add_action('wp_head', 'opengraphsingle');
 function opengraphsingle(){
 	if (is_single()) {
@@ -16,5 +18,35 @@ function opengraphsingle(){
 			$image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
 			echo '<meta property="og:image" content="',$image[0],'"/>';
 		}
+		else {
+			echo '<meta property="og:image" content="',esc_attr(get_option( 'default-image' )),'"/>';
+		}
 	}
 }
+
+// Settings Menu
+add_action('admin_menu', 'opengraphsingle_menu');
+function opengraphsingle_menu(){
+	add_menu_page( 'Facebook Single Posts OG', 'Facebook OG', 'administrator', 'fb-opengraph-tags', 'opengraphsingle_menu_page', 'dashicons-facebook', '50' );
+}
+
+add_action( 'admin_init', 'opengraph_options' );
+
+function opengraph_options(){
+	register_setting( 'meta-data', 'default-image' );
+}
+
+function opengraphsingle_menu_page(){ ?>
+	<div class="wrap">
+		<h1> Facebook OG Tags Settings </h1>
+		<form action="options.php" method="post">
+		<?php settings_fields( 'meta-data' ); ?>
+		<?php do_settings_sections( 'opengraphsingle_menu' ); ?>
+		<label> Default Image URL </label>
+		<input type="text" name="default-image" value="<?php echo esc_attr( get_option( 'default-image' )); ?>">
+		<?php submit_button(); ?>
+		</form>
+	</div>
+	<?php
+	}
+	?>
---
+++ facebook-opengraph-tags/readme.txt
@@ -0,0 +1,8 @@
+This plugin includes the Facebook OG tags into your blog's single posts which include Blog Title, Post Title, Description and Featured Image (if available). If you're facing issues in the thumbnails while pasting your blog post link on Facebook, this plugin is must for you. 
+
+# Note: 
+Facebook OG Tags will be added only on Single Posts.
+
+- We do not add any backlink; it's a fully open source plugin. Source code can be found on Git: https://github.com/anonymousguyx/facebook-opengraph-tags
+
+Thank you!
